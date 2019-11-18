33d8b5b4cfdf99dc492d66172b4ed2add8489136
taehyuk choi <chlxogur_@naver.com>
2019-11-19 06:25:58 +0900

    TaeHyeok

--- Donggora/votingDapp/urls.py
+++ Donggora/votingDapp/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='main'),
+    path('login', views.login, name='login'),
+    path('vote', views.vote, name='vote'),
+    path('findpw', views.findpw, name='findpw'),
+    path('tables', views.tables, name='tables'),
+    path('signup', views.signup, name='signup'),
+    path('mypage', views.mypage, name='mypage'),
+    path('makeVote', views.makeVote, name='makeVote'),
 ]
--- Donggora/votingDapp/views.py
+++ Donggora/votingDapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def index(request):
 	return render(request, "main.html")
+def findpw(request):
+	return render(request, "findpw.html")
+def login(request):
+	return render(request, "login.html")
+def signup(request):
+	return render(request, "signup.html")
+def tables(request):
+	return render(request, "tables.html")
+def vote(request):
+	return render(request, "vote.html")
+def mypage(request):
+	return render(request, "mypage.html")
+def makeVote(request):
+	return render(request, "makeVote.html")
